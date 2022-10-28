39329e9d640bb89dd791e427d800c4b1a6530076
DiogoB0mfim <diogohbomfim@gmail.com>
2022-10-28 18:30:32 -0300

    teste de responsividade

--- buffallos/src/pages/Main/Main.js
+++ buffallos/src/pages/Main/Main.js
@@ -1,21 +1,26 @@
-import { Container, SubTitle, Texts } from "./MainStyled"
-import image from "../../assets/background-main.png"
-import { ImageDesktop } from "./MainStyled"
-import { Title } from "./MainStyled"
-import { ImageMobile } from "./MainStyled"
-import backgoundblack from "../../assets/backgroundMobile.png"
+import { Container, SubTitle, Texts } from "./MainStyled";
+import image from "../../assets/background-main.png";
+import { ImageDesktop } from "./MainStyled";
+import { Title } from "./MainStyled";
+import { ImageMobile } from "./MainStyled";
+import backgoundblack from "../../assets/backgroundMobile.png";
+import { ImgWpp } from "./MainStyled";
+import wpp from "../../assets/botaoWpp.png";
 
 const Main = () => {
-    return (
-        <Container>
-            <ImageDesktop src={image}/>
-            <ImageMobile src={backgoundblack}/>
-            <Texts>
-            <Title>BUFFALLOS BAR</Title>
-            <SubTitle>Cerveja, Drinks, Comida boa e Rock</SubTitle>
-            </Texts>          
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <ImageDesktop src={image} />
+      <ImageMobile src={backgoundblack} />
+      <Texts>
+        <Title>BUFFALLOS BAR</Title>
+        <SubTitle>Cerveja, Drinks, Comida boa e Rock</SubTitle>
+      </Texts>
+        <a href="https://wa.me/+5521976460392?text=Reservas" target="blank">
+          <ImgWpp src={wpp} alt="botão do whatsapp" />
+        </a>
+    </Container>
+  );
+};
 
-export default Main
+export default Main;
