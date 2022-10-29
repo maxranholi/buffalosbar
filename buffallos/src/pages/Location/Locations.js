9a12422d0b195d013e9eefa21ffbed8144114c3a
DiogoB0mfim <diogohbomfim@gmail.com>
2022-10-29 17:04:03 -0300

    localização finalizada

--- buffallos/src/pages/Location/Locations.js
+++ buffallos/src/pages/Location/Locations.js
@@ -1,12 +1,43 @@
-import { Container } from "./LocationStyled";
+import * as S from "./LocationStyled";
 
 const Location = () => {
-    return(
-<div>
-        <Container>Localização</Container>
-    </div>
-    )
-    
-}
+  return (
+    <>
+      <S.SectionTitle>Localização</S.SectionTitle>
+      <S.Container>
+        <S.ContainerInfos>
+          <div>
+            <S.TitleLoc>Onde estamos</S.TitleLoc>
+            <S.PLoc>Rua Arquias Cordeiro, N 316. Méier</S.PLoc>
+          </div>
+          <div>
+            <S.TitleLoc>Funcionamento</S.TitleLoc>
+            <div>
+              <S.PLoc>
+                <b>Sexta:</b> A partir das 21:00h
+              </S.PLoc>
+              <S.PLoc>
+                <b>Sábado:</b> A partir das 20:00h
+              </S.PLoc>
+            </div>
+          </div>
+        </S.ContainerInfos>
 
-export default Location
+        <div class="mapouter">
+          <div class="gmap_canvas">
+            <S.IframeMap
+              class="gmap_iframe"
+              frameborder="0"
+              scrolling="no"
+              marginheight="0"
+              marginwidth="0"
+              src="https://maps.google.com/maps?width=660&amp;height=500&amp;hl=en&amp;q=arquias cordeiro, 316 meier Buffalos Bar&amp;t=&amp;z=18&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+            />
+          </div>
+        </div>
+      </S.Container>
+    </>
+  );
+};
+
+export default Location;
